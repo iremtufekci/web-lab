115d5797dcd98ec704b2fcacae5daa5abf5722fa
Irem Tufekci <tufekcirem9@gmail.com>
2026-02-17 11:17:55 +0300

    chore: initial project setup with Vite + React + TS

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <h1>Web Tasarımı ve Programlama</h1>
+      <h2>LAB-1</h2>
+      <p>Ad Soyad: İrem Tüfekçi</p>
+      <p>Öğrenci No: [230541040]</p>
+    </div>
+  );
+}
+
+export default App;
